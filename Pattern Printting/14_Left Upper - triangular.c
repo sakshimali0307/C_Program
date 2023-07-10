b24d0bf66130f6f1faf2abae2d029c094f79114a
sakshimali0307 <127278584+sakshimali0307@users.noreply.github.com>
2023-07-10 19:24:35 +0530

    Pattern Printtind

---
+++ Pattern Printting/14_Left Upper - triangular.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+  int i =0, j = 0, X = 0;
+
+  printf("\nEnter row Size =");
+  scanf("%d",&X);
+
+  printf("\n\n pattern is >=\n\n");
+
+  for(i = 1; i <= X; i++)
+  {
+    for(j = 1; j <= X; j++)
+    {
+      if (i+j <= X+1)
+      {
+     printf(" * ");
+    }
+    else
+    {
+      printf("   ");
+    }
+  }
+  printf("\n");
+  }
+
+ getch();
+ return 0;
+ }
