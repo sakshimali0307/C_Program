3158f3779e258ed329b127f20cd295eeb30a5f7d
sakshimali0307 <127278584+sakshimali0307@users.noreply.github.com>
2023-08-27 21:45:06 +0530

    string - function

---
+++ String in Function/01_A_string length using function.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<conio.h>
+
+int strlenX(char[20]);
+
+int main()
+{
+    char cSrc[20]={'\0'};
+    int len = 0;
+
+    printf("\n Enter a string :");
+    gets(cSrc);
+
+    len = strlenX(cSrc);
+
+    printf("\n lenght of given string is = %d",len);
+
+    getch();
+    return 0;
+
+    }
+int strlenX(char cArr[])
+{
+    int cnt = 0;
+
+    printf("\n size = %d.\n", sizeof(cArr));
+
+    while(cArr[cnt] != '\0')
+    {
+      cnt++;
+    }
+
+    return cnt;
+}
