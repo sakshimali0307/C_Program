33f4013b331eac47daebbc0199b2374d57fbf2be
sakshimali0307 <127278584+sakshimali0307@users.noreply.github.com>
2023-11-11 21:52:21 +0530

    Case Study

---
+++ Case Study/v04 - student Info.c
@@ -0,0 +1,98 @@
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+#include<string.h>
+
+struct stud
+{
+    int Roll_No;
+    char Name[40];
+    int Phy;
+    int Chem;
+    int Maths;
+    float Total;
+    float Per;
+};
+
+
+int main()
+{
+
+    struct stud std[3];
+
+    void Accept_Student_Details(struct stud*);
+    void Display_Student_Details(struct stud*);
+
+
+    for(int i = 0; i < 3; i++)
+    {
+        printf("\n Enter student %d Details =>\n",i+1);
+        Accept_Student_Details(&std[i]);
+    }
+    printf("\n Enter Any Key To Go Ahead =>\n");
+    getch();
+
+    printf("\n===========***********==============\n");
+
+    printf("\n Entered Students Details Are =>\n\n");
+
+    for(int i = 0; i < 3; i++)
+    {
+        printf("\n\n %d Student Details =>\n",i+1);
+        Display_Student_Details(&std[i]);
+    }
+
+    printf("\n\n Thanks For Using Our Application !!!");
+
+    getch();
+    return 0;
+}
+
+void Accept_Student_Details(struct stud *sPtr)
+{
+    printf("\n Enter student Details =>\n");
+
+    printf("\n Enter Student Roll No : ");
+    scanf("%d",&sPtr->Roll_No);
+
+    fflush(stdin);
+
+    printf("\n Enter Student Name :");
+    gets(sPtr->Name);
+
+    printf("\n Enter Physics Marks :");
+    scanf("%d",&sPtr->Phy);
+
+    printf("\n Enter Chemistry Marks :");
+    scanf("%d",&sPtr->Chem);
+
+    printf("\n Enter Mathematics Marks :");
+    scanf("%d",&sPtr->Maths);
+
+     sPtr->Total = sPtr->Phy + sPtr->Chem + sPtr->Maths;
+
+    sPtr->Per = (sPtr->Total / 300)*100;
+
+    return;
+}
+void Display_Student_Details(struct stud *sPtr)
+{
+    printf("\n=====================================\n");
+
+    printf("\n\n Entered students Details Are =>\n\n");
+
+    printf("\n\t\t Roll Number       = %d",sPtr->Roll_No);
+    printf("\n\t\t Student Name      = %s",sPtr->Name);
+    printf("\n\t\t Physics Marks     = %d",sPtr->Phy);
+    printf("\n\t\t Chemistry Marks   = %d",sPtr->Chem);
+    printf("\n\t\t Mathematics Marks = %d",sPtr->Maths);
+    printf("\n\t\t Total Marks       = %0.0f",sPtr->Total);
+    printf("\n\t\t Percentage        = %0.3f",sPtr->Per);
+
+    printf("\n\n=============********===============\n");
+
+
+    getch();
+    return;
+}
+
